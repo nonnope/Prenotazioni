201794e4d0752fe3fec7d52684facd1d6b1f574f
nonnope <74015892+nonnope@users.noreply.github.com>
2025-01-28 18:43:13 +0100

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -18,7 +18,7 @@ class Config:
     SMTP_USERNAME = os.getenv("SMTP_USERNAME")
     SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
     SMTP_SERVER = os.getenv("SMTP_SERVER")
-    SMTP_PORT = int(os.getenv("SMTP_PORT"))
+    SMTP_PORT = int(os.getenv("SMTP_PORT" 587))
     EMAIL_ALIAS = os.getenv("EMAIL_ALIAS", None)
 
     # Configurazioni generali
